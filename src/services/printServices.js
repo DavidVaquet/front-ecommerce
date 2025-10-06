f1417ebe2375843ef09fa17e628785edf9c51220
David Vaquet <143475363+DavidVaquet@users.noreply.github.com>
2025-10-06 11:54:08 -0300

    f

--- src/services/printServices.js
+++ src/services/printServices.js
@@ -1,9 +1,10 @@
-import { apiFetch } from "../helpers/auth";
+const API_KEY = import.meta.env.API_KEY;
 const API_URL = `${import.meta.env.VITE_API_URL}/print`;
 export const printEtiqueta = async({producto, ancho, alto, copias, mode}) => {
     try {
-        const res = await apiFetch(`${API_URL}`, {
+        const res = await fetch(`${API_URL}`, {
             method: 'POST',
+            headers: {'x-api-key': API_KEY, 'Content-Type': 'application/json'},
             body: JSON.stringify({ producto, ancho, alto, copias, mode})
         });
 
